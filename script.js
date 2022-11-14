66ed9e6e7d4a12d3ceeef2f2a97f0aaac447b8d1
nushon <nushokarmo@gmail.com>
2022-11-14 01:12:30 +0000

    Created index.html, styles.css, home.html, home.css, login.html, login.css,script.js pages and images folder

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var mobileDiv = document.getElementById("mobileDiv");
+    if (mobileDiv.style.display === "block") {
+        mobileDiv.style.display = "none";
+    } else {
+        mobileDiv.style.display = "block";
+    }
+  }
