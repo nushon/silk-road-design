9ef23dc671d3ff85e3f6582dd51251a624e17169
nushon <nushokarmo@gmail.com>
2022-11-28 03:46:50 +0000

    Updated

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function mobileView() {
-    var mobileDiv = document.getElementById("mobileDiv");
+    let mobileDiv = document.getElementById("mobileDiv");
+    let mobileLinkMainDiv = document.getElementById("mobileLinkMainDiv");
     if (mobileDiv.style.display === "block") {
         mobileDiv.style.display = "none";
+        mobileLinkMainDiv.display = "none";
     } else {
         mobileDiv.style.display = "block";
+        mobileLinkMainDiv.display = "block";
     }
   }
